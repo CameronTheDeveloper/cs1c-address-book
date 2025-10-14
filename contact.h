553ad3aaf0483cc9f86e9396cb77ce6387deebe6
Rat <58471843+RatTaco@users.noreply.github.com>
2025-10-14 16:57:33 -0700

    Add contact.h definition

---
+++ contact.h
@@ -0,0 +1,38 @@
+#ifndef CONTACT_H
+#define CONTACT_H
+#include <string>
+#include <vector>
+
+class Contact
+{
+private:
+    std::string name;
+    std::string phoneNumber;
+    std::string email;
+    std::string contactType;
+    std::string city;
+    std::string group;
+    std::vector<std::string> tags;
+
+public:
+    Contact(const std::string &name, const std::string &phoneNumber, const std::string &email, const std::string &contactType);
+    virtual ~Contact();
+    bool contactTypeIsValid(const std::string &contactTypeInput);
+    void setName(const std::string &newName);
+    void setPhoneNumber(const std::string &newPhoneNum);
+    void setEmail(const std::string &newEmail);
+    void setContactType(const std::string &newContactType);
+    void setCity(const std::string &newCity);
+    void setGroup(const std::string &newGroup);
+    std::string getName();
+    std::string getPhoneNumber();
+    std::string getEmail();
+    std::string getContactType();
+    std::string getCity();
+    std::string getGroup();
+
+    void addTag(const std::string &newTag);
+    void removeTag(const std::string &tagToRemove);
+};
+
+#endif // CONTACT_H
