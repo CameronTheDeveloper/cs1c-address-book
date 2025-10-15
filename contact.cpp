99861b3cbd8756eefa0992f76c2be7aad65d6d82
Rat <58471843+RatTaco@users.noreply.github.com>
2025-10-14 19:18:26 -0700

    Add contact.cpp setter implementations

--- contact.cpp
+++ contact.cpp
@@ -6,3 +6,35 @@
 Contact::Contact(const std::string &name, const std::string &phoneNumber, const std::string &email,
                  const std::string &city, const std::string &group, const std::string &address)
     : name(name), phoneNumber(phoneNumber), email(email), city(city), group(group), address(address) {}
+
+Contact::~Contact() = default;
+
+void Contact::setName(const std::string &newName)
+{
+    name = newName;
+}
+
+void Contact::setPhoneNumber(const std::string &newPhoneNum)
+{
+    phoneNumber = newPhoneNum;
+}
+
+void Contact::setEmail(const std::string &newEmail)
+{
+    email = newEmail;
+}
+
+void Contact::setCity(const std::string &newCity)
+{
+    city = newCity;
+}
+
+void Contact::setGroup(const std::string &newGroup)
+{
+    group = newGroup;
+}
+
+void Contact::setAddress(const std::string &newAddress)
+{
+    address = newAddress;
+}
