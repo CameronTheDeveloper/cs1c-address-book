caf345bba0d65e8088773680d1c70c1f975ad77a
Cameron <58471843+CameronTheDeveloper@users.noreply.github.com>
2025-10-16 01:31:57 -0700

    Fix getDateOfBirth decleration

--- person.h
+++ person.h
@@ -10,7 +10,7 @@ class Person : public Contact
         Person(const std::string &name, const std::string &phoneNumber, const std::string &email,
             const std::string &city, const std::string &group, const std::string &address, const std::string &dateOfBirth);
 
-        std::string getDateOfBirth() const;
+        const std::string &getDateOfBirth() const;
         void setDateOfBirth(const std::string &dateOfBirth);
 
         std::string getType() const override; 
