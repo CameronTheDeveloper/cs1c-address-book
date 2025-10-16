a8752f4b8b3826dc5de24f43022d3c683f8af303
Cameron <58471843+CameronTheDeveloper@users.noreply.github.com>
2025-10-16 04:06:17 -0700

    Add filterByTag and filterByCity

--- addressbook.h
+++ addressbook.h
@@ -13,10 +13,12 @@ class AddressBook {
 
         // Methods for Search and Filtering
         std::vector<const Contact*> searchByName(const std::string &nameQuery) const;
-        std::vector<const Contact*> filterByType(const std::string &type) const;
         std::vector<const Contact*> searchByPhoneNumber(const std::string& phoneQuery) const;
         std::vector<const Contact*> searchByEmail(const std::string& emailQuery) const;
 
+        std::vector<const Contact*> filterByType(const std::string &type) const;
+        std::vector<const Contact*> filterByCity(const std::string& city) const;
+        std::vector<const Contact*> filterByTag(const std::string& tag) const;
     private:
         std::vector<std::unique_ptr<Contact>> contacts; 
 };
